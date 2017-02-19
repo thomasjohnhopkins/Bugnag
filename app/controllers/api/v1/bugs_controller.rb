4ebe066fc1740304943072e44b6f7106fd9c2a23
thomasjohnhopkins <thomas.john.hopkins@gmail.com>
2017-02-18 21:32:17 -0500

    Clean up routes in users and bugs controllers.

--- app/controllers/api/v1/bugs_controller.rb
+++ app/controllers/api/v1/bugs_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::BugsController < ApplicationController
-  before_action :require_current_bug!, except: :create
 
   def create
     @bug = Bug.new(bug_params)
 
     if @bug.save!
-      render :show
+      render 'api/bugs/show.json.jbuilder'
     else
       render json: "Sorry. Display some error message here."
     end
@@ -14,20 +13,20 @@ class Api::V1::BugsController < ApplicationController
   def index
     @bugs = Bug.all
 
-    render :index
+    render 'api/bugs/index.json.jbuilder'
   end
 
   def show
     @bug = Bug.find_by(params[id])
 
-    render :show
+    render 'api/bugs/show.json.jbuilder'
   end
 
   def update
     @bug = Bug.find_by_id(params[:id])
 
     if @bug.update(bug_params)
-      render :show
+      render 'api/bugs/show.json.jbuilder'
     else
       render json: @bug.errors.full_messages
     end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :require_current_user!, except: :create
+  # want to add this eventually
+  # before_action :require_current_user!, except: :create
 
   def create
     @user = User.new(user_params)
 
     if @user.save!
       log_in!(@user)
-      render :show
+      render 'api/users/show.json.jbuilder'
     else
       render json: "Sorry. Display some error message here."
     end
@@ -15,20 +16,20 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render :index
+    render 'api/users/index.json.jbuilder'
   end
 
   def show
     @user = User.find_by(params[id])
 
-    render :show
+    render 'api/users/show.json.jbuilder'
   end
 
   def update
     @user = User.find_by_id(params[:id])
 
     if @user.update(user_params)
-      render :show
+      render 'api/users/show.json.jbuilder'
     else
       render json: @user.errors.full_messages
     end
